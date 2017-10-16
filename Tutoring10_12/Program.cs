12110f886d0f5e7f0891a99c65f2cc33f1a74fbb
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-16 14:56:28 -0400

    initial commit

---
+++ Tutoring10_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutoring10_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
